878b49348d9ed388450ffbb4f14b575abb4dd2c7
Lesly Polo <128642999+leslypoloascue@users.noreply.github.com>
2024-06-08 22:55:33 -0500

    Update PA_Matricula.sql

--- BD/PA_Matricula.sql
+++ BD/PA_Matricula.sql
@@ -1,59 +1,128 @@
--- Usar la base de datos
 USE universidad;
 GO
 
+IF OBJECT_ID('spListarMatricula') IS NOT NULL
+    DROP PROC spListarMatricula;
+GO
 
--- Procedimiento almacenado para agregar una matrÌcula
+CREATE PROC spListarMatricula
+AS
+BEGIN
+    SELECT * FROM matricula;
+END;
+GO
+
 IF OBJECT_ID('spAgregarMatricula') IS NOT NULL
     DROP PROC spAgregarMatricula;
 GO
 
 CREATE PROC spAgregarMatricula
-    @periodo NVARCHAR(6),
-    @promedio INT,
-    @CourseID INT,
-    @StudentID INT
+@periodo NVARCHAR(6),
+@promedio INT,
+@CourseID INT,
+@StudentID INT
 AS
 BEGIN
-    -- LÛgica de tu procedimiento aquÌ
-    -- Por ejemplo, insertar los datos en la tabla de matrÌcula
-    INSERT INTO matricula (periodo, promedio, CourseID, StudentID)
-    VALUES (@periodo, @promedio, @CourseID, @StudentID);
-END
+    -- Verificar si ya existe una matr√≠cula con el mismo periodo, CourseID y StudentID
+    IF NOT EXISTS (SELECT * FROM matricula WHERE periodo = @periodo AND CourseID = @CourseID AND StudentID = @StudentID)
+    BEGIN
+        BEGIN TRY
+            INSERT INTO matricula (periodo, promedio, CourseID, StudentID)
+            VALUES (@periodo, @promedio, @CourseID, @StudentID);
+            SELECT CodError = 0, Mensaje = 'Matr√≠cula insertada correctamente';
+        END TRY
+        BEGIN CATCH
+            SELECT CodError = 1, Mensaje = 'Error: No se pudo agregar la matr√≠cula';
+        END CATCH
+    END
+    ELSE
+        SELECT CodError = 1, Mensaje = 'Error: Ya existe una matr√≠cula con el mismo periodo, CourseID y StudentID';
+END;
 GO
+-- Ejecutar el procedimiento almacenado spAgregarMatricula
 
--- Procedimiento almacenado para eliminar una matrÌcula
+
+
 IF OBJECT_ID('spEliminarMatricula') IS NOT NULL
     DROP PROC spEliminarMatricula;
 GO
 
+
 CREATE PROC spEliminarMatricula
-    @EnrollmentID INT
+@EnrollmentID INT
 AS
 BEGIN
-    -- LÛgica de tu procedimiento aquÌ
-    -- Por ejemplo, eliminar una matrÌcula basada en el EnrollmentID
-    DELETE FROM matricula WHERE EnrollmentID = @EnrollmentID;
+    -- Verificar que el EnrollmentID existe en la tabla Matricula
+    IF EXISTS (SELECT EnrollmentID FROM matricula WHERE EnrollmentID = @EnrollmentID)
+    BEGIN
+        BEGIN TRAN tranEliminar
+        BEGIN TRY
+            DELETE FROM matricula WHERE EnrollmentID = @EnrollmentID
+            COMMIT TRAN tranEliminar
+            SELECT CodError = 0, Mensaje = 'Matr√≠cula eliminada correctamente'
+        END TRY
+        BEGIN CATCH
+            ROLLBACK TRAN tranEliminar
+            SELECT CodError = 1, Mensaje = 'Error: No se ejecut√≥ la transacci√≥n'
+        END CATCH
+    END
+    ELSE
+        SELECT CodError = 1, Mensaje = 'Error: EnrollmentID no existe en la Matr√≠cula'
 END
 GO
 
--- Procedimiento almacenado para actualizar la informaciÛn de una matrÌcula
+
 IF OBJECT_ID('spActualizarMatricula') IS NOT NULL
     DROP PROC spActualizarMatricula;
 GO
 
-CREATE PROC spActualizarMatricula
+
+
+CREATE PROCEDURE spActualizarMatricula
+@EnrollmentID INT,
+@periodo NVARCHAR(6),
+@promedio INT,
+@CourseID INT,
+@StudentID INT
+AS
+BEGIN
+    -- Verificar si la matr√≠cula existe en la tabla Matricula
+    IF EXISTS (SELECT * FROM matricula WHERE EnrollmentID = @EnrollmentID)
+    BEGIN
+        BEGIN TRY
+            UPDATE Matricula 
+            SET periodo = @periodo, 
+                promedio = @promedio, 
+                CourseID = @CourseID, 
+                StudentID = @StudentID 
+            WHERE EnrollmentID = @EnrollmentID;
+            SELECT CodError = 0, Mensaje = 'Matr√≠cula actualizada correctamente';
+        END TRY
+        BEGIN CATCH
+            SELECT CodError = 1, Mensaje = 'Error: No se pudo actualizar la matr√≠cula';
+        END CATCH
+    END
+    ELSE
+        SELECT CodError = 1, Mensaje = 'Error: La matr√≠cula no existe en la tabla Matricula';
+END;
+GO
+
+
+
+IF OBJECT_ID('sp_BuscarMatriculaPorParametros') IS NOT NULL
+    DROP PROCEDURE sp_BuscarMatriculaPorParametros;
+GO
+
+CREATE PROCEDURE sp_BuscarMatriculaPorParametros
     @EnrollmentID INT,
-    @periodo NVARCHAR(6),
-    @promedio INT,
     @CourseID INT,
     @StudentID INT
 AS
 BEGIN
-    -- LÛgica de tu procedimiento aquÌ
-    -- Por ejemplo, actualizar los datos de una matrÌcula basada en el EnrollmentID
-    UPDATE matricula 
-    SET periodo = @periodo, promedio = @promedio, CourseID = @CourseID, StudentID = @StudentID 
-    WHERE EnrollmentID = @EnrollmentID;
-END
+    SELECT * 
+    FROM matricula
+    WHERE EnrollmentID = @EnrollmentID
+      AND CourseID = @CourseID
+      AND StudentID = @StudentID;
+END;
 GO
